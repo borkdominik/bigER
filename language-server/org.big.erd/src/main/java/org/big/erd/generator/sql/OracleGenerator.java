2114154b1f97ee6d3d3d6904557e0a5419760a28
Christoph Lauscher <christoph.lauscher@gmail.com>
2022-11-20 23:13:30 +0100

    SQL generator: Oracle

---
+++ language-server/org.big.erd/src/main/java/org/big/erd/generator/sql/OracleGenerator.java
@@ -0,0 +1,18 @@
+package org.big.erd.generator.sql;
+
+import org.big.erd.entityRelationship.DataType;
+
+/**
+ * Generates SQL for MS SQL Server from the ER model.
+ */
+public class OracleGenerator extends SqlGenerator {
+	
+	@Override
+	protected String getDataType(DataType dataType) {
+		String type = super.getDataType(dataType);
+		if ("DOUBLE".equalsIgnoreCase(type)) {
+			return "DECIMAL";
+		}
+		return type;
+	}
+}
