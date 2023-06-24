20755f694bbc865b04501d8903568f9f51b00e2f
Christoph Lauscher <christoph.lauscher@gmail.com>
2023-06-24 15:43:09 +0200

    multi-notation support - UML

---
+++ language-server/org.big.erd/src/main/java/org/big/erd/generator/sql/UmlSqlImport.java
@@ -0,0 +1,13 @@
+package org.big.erd.generator.sql;
+
+public class UmlSqlImport extends SqlImport {
+
+	protected String getMinimumCardinality(boolean isMandatory) {
+		return isMandatory ? "1..." : "0...";
+	}
+
+	protected String getMaximumCardinality(boolean isSingle) {
+		return isSingle ? "1" : "*";
+	}
+
+}
