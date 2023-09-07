9be478132a90b4759fd80beb92cff6a5e428adfc
plglaser <plg741@gmail.com>
2023-09-08 00:43:34 +0200

    adapt client-side code

--- extension/src/commands/import.ts
+++ extension/src/commands/import.ts
@@ -1,12 +1,12 @@
 import { window, Uri, commands, workspace } from "vscode";
 import * as os from 'os';
+import * as path from 'path';
 
 export const IMPORT_COMMAND = "erdiagram.import.sql";
 
 export const importSqlHandler = async () => {
-    // verify .erd file is currently open
     const activeTextEditor = window.activeTextEditor;
-    const erdFileUri = activeTextEditor?.document?.languageId === 'erdiagram' 
+    const erdFileUri = activeTextEditor?.document?.languageId === 'erdiagram'
         ? activeTextEditor.document.uri
         : undefined;
     if (!erdFileUri || erdFileUri === undefined) {
@@ -14,12 +14,9 @@ export const importSqlHandler = async () => {
         return;
     }
 
-    // set up default URI for file selection
     const defaultUri = workspace.workspaceFolders && workspace.workspaceFolders.length > 0
 		? Uri.file(workspace.workspaceFolders[0].uri.fsPath)
 		: Uri.file(os.homedir());
-    
-    // open file selection dialog
     const result = await window.showOpenDialog({
 		canSelectFiles: true,
 		canSelectFolders: false,
@@ -30,14 +27,18 @@ export const importSqlHandler = async () => {
 	if (!result || result.length === 0) {
 		return;
 	}
-    
+
     const sqlFileUri = result[0];
-    // verify file extension of selected file
     if (!sqlFileUri?.fsPath.endsWith('sql')) {
         window.showErrorMessage("Error! Invalid file.");
         return;
     }
-    
+    // verify .erd and .sql file are in the same directory
+    // TODO: only tested on macOS -> check on windows + linux platforms
+    if (path.dirname(erdFileUri.fsPath) !== path.dirname(sqlFileUri.fsPath)) {
+        window.showErrorMessage("Error! File should be in the same directory as the current .erd file.");
+        return;
+    }
     // send command with .erd file URI (param 1) and .sql file URI (param 2) to server
     const response = await commands.executeCommand<string>(IMPORT_COMMAND, erdFileUri.toString(), sqlFileUri.toString());
     if (response === undefined) {
